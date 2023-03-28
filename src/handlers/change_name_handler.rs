ff158214b20ef8a1af102d4bfad8337343ad9210
NoteAP00 <apinun893@gmail.com>
2023-03-28 14:26:44 +0700

    add Access-Control-Allow

--- src/handlers/change_name_handler.rs
+++ src/handlers/change_name_handler.rs
@@ -1,34 +1,45 @@
-use crate::models::bank::{ChangeName,DataChange}; //bank.rs
-use actix_web::{put,web,HttpResponse,Responder};
-use serde::{Serialize,Deserialize};
+use crate::models::bank::{ChangeName, DataChange}; //bank.rs
+use actix_web::{put, web, HttpResponse, Responder};
+use serde::{Deserialize, Serialize};
 use serde_json::json;
 #[derive(Serialize, Deserialize)]
 struct Headers {
-    new_name:String,
+    new_name: String,
 }
 #[put("/change_name/{id}")]
-async fn change_name(account_id: web::Path<i32>,change_name: web::Json<Headers>)  -> impl  Responder {
+async fn change_name(
+    account_id: web::Path<i32>,
+    change_name: web::Json<Headers>,
+) -> impl Responder {
     let inner: Headers = change_name.into_inner();
     let id = account_id.to_string().parse().unwrap();
     let name = inner.new_name.to_string();
 
     if id == 1212312121 {
-        let y : DataChange = DataChange { // models/bank.rs
-            message:("Edit Success!").to_string(),
-            account_id:id,
-            account_name:name.to_string(), 
-            };
-            let change_name = ChangeName { // models/bank.rs
-            message:("Edit account_name").to_string(),
-            data:vec![y],
-            };
-            HttpResponse::Ok().json(change_name)
-        }
-        else {
-            HttpResponse::NotFound().json(json!({
+        let y: DataChange = DataChange {
+            // models/bank.rs
+            message: ("Edit Success!").to_string(),
+            account_id: id,
+            account_name: name.to_string(),
+        };
+        let change_name = ChangeName {
+            // models/bank.rs
+            message: ("Edit account_name").to_string(),
+            data: vec![y],
+        };
+        HttpResponse::Ok()
+            .header("Access-Control-Allow-Origin", "*")
+            .header("Access-Control-Allow-Headers", "Content-Type")
+            .header("Access-Control-Allow-Methods", "PUT, OPTIONS")
+            .json(change_name)
+    } else {
+        HttpResponse::NotFound()
+            .header("Access-Control-Allow-Origin", "*")
+            .header("Access-Control-Allow-Headers", "Content-Type")
+            .header("Access-Control-Allow-Methods", "PUT, OPTIONS")
+            .json(json!({
                 "message": "User not found!",
                 "data": null
             }))
-       
     }
-    }
+}
